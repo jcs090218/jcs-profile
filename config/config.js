9aaec21a3d6a6bf7d126ae389f92b47ea8db72e2
jenchieh <jcs090218@gmail.com>
2018-10-24 00:13:14 +0800

    Add website config.

---
+++ config/config.js
@@ -0,0 +1,12 @@
+/**
+ * $File: config.js $
+ * $Date: 2017-09-17 13:17:44 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright (c) 2017 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+var announcement = "Love programming, focus on Software Dev!~";
