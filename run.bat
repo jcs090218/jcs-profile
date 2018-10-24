0653a79ace20e26ba6a5bce3cb83848ec20e79fd
jenchieh <jcs090218@gmail.com>
2018-10-25 00:37:05 +0800

    Add util.

---
+++ run.bat
@@ -0,0 +1,15 @@
+:: ========================================================================
+:: $File: run.bat $
+:: $Date: 2017-09-17 13:13:13 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                    Copyright (c) 2017 by Shen, Jen-Chieh $
+:: ========================================================================
+
+
+echo.
+
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "%~dp0%\index.html"
+
+echo.
