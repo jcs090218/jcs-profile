59822b54ce67acf1a45170d1005fc7d657234a6a
jenchieh <jcs090218@gmail.com>
2019-04-23 00:17:24 +0800

    Add unix util.

---
+++ scripts/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ========================================================================
+# $File: run.sh $
+# $Date: 2019-04-23 00:15:29 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2019 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+cd ..
+
+echo
+
+sensible-browser ./index.html
+
+echo
